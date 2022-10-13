1c1788602df84e497bc91819282cc9a58e6493da
Ilona Lapka <ilona2000123@wp.pl>
2022-10-13 21:08:07 +0200

    Initial

---
+++ Color.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytm15
+{
+    //[{"nameId":4473290,"hexCode":"ac7bd1","name":"Velvet Purple Flower"},{
+    //"nameId":44732
+    public class Color
+    {
+        public long NameId { get; set; }
+        public string HexCode { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+
+namespace Algorytm15
+{
+    internal class Program
+    {
+        static HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Hexcolor(255, 99, 71));
+            Console.WriteLine(Hexcolor(184, 134, 11));
+            Console.WriteLine(Hexcolor(189, 183, 107));
+            Console.WriteLine(Hexcolor(0, 0, 205));
+            Blend(new List<string>() { "#000000", "#778899" });
+            Blend(new List<string>() { "#E6E6FA", "#FF69B4", "#B0C4DE" });
+        }
+
+        static string Hexcolor(byte red, byte green, byte blue)
+        {
+            string hexString = Convert.ToHexString(new byte[] { red, green, blue });
+            var response = client.GetStringAsync($"https://colornames.org/search/json/?hex={hexString}").Result;
+
+            Color c = JsonConvert.DeserializeObject<Color>(response);
+            return "#" +  hexString + " " + c.Name;
+        }
+
+        static void Blend(List<string> colors)
+        {
+            int sumRed, sumGreen, sumBlue;
+            sumRed = sumGreen = sumBlue = 0;
+            foreach (var item in colors)
+            {
+                byte[] c = Convert.FromHexString(item[1..]);
+                sumRed += Convert.ToInt32(c[0]);
+                sumGreen += Convert.ToInt32(c[1]);
+                sumBlue += Convert.ToInt32(c[2]);
+            }
+            double red = Convert.ToDouble(sumRed) / colors.Count;
+            double green = Convert.ToDouble(sumGreen) / colors.Count;
+            double blue = Convert.ToDouble(sumBlue) / colors.Count;
+            Console.WriteLine(Hexcolor(Convert.ToByte(Math.Round(red)), 
+                Convert.ToByte(Math.Round(green)), 
+                Convert.ToByte(Math.Round(blue))));
+        }
+    }
+}
